9862e83a7acbd45de12346fa09dac16822429edd
Sandivya Saxena <saxenasandivya@gmail.com>
2020-10-24 19:13:27 +0530

    GuessMyNumber baseline

---
+++ script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const SECRET_NUMBER = Math.floor(Math.random() * 20) + 1;
+let score = 10;
+let highScore = Number(localStorage.getItem('highScore')) || 0
+
+document.querySelector('.highscore').textContent = highScore
+const decrementScore = () => (--score);
+
+const againHandler = () => {
+  return location.reload()
+};
+
+const guessNumberHandler = () => {
+  const guessedNumber = Number(document.querySelector('.guess').value);
+
+  if (score < 1) {
+    document.querySelector('.message').textContent = 'Maximum tries exceeded, click Again!';
+  } else {
+    if (!guessedNumber) {
+      document.querySelector('.message').textContent = 'Put in your best guess.';
+    }
+    else if (guessedNumber < 1 || guessedNumber > 20) {
+      document.querySelector('.message').textContent = 'Guess between 1 and 20.';
+    }
+    else if (guessedNumber > SECRET_NUMBER) {
+      document.querySelector('.message').textContent = 'Too High!';
+      document.querySelector('.score').textContent = decrementScore();
+    }
+    else if (guessedNumber < SECRET_NUMBER) {
+      document.querySelector('.message').textContent = 'Too Low!';
+      document.querySelector('.score').textContent = decrementScore();
+    }
+    else {
+      document.querySelector('.message').textContent = 'Correct Number!';
+      document.querySelector('.number').textContent = SECRET_NUMBER;
+      document.querySelector('body').style.backgroundColor = '#60b347';
+      document.querySelector('.number').style.width = '30rem';
+
+      if (score > highScore) {
+        localStorage.setItem('highScore', score);
+        highScore = score;
+        document.querySelector('.highscore').textContent = highScore;
+      }
+    }
+  }
+
+};
+
+document.querySelector('.check').addEventListener('click', guessNumberHandler);
+document.querySelector('.again').addEventListener('click', againHandler)
